0645e2bbcad2c7ce6100cfcbb72a8554b1a4ed76
Zeus432 <msiddhu@outlook.com>
2021-10-30 12:43:14 +0530

    Add source command

--- Cogs/botstuff/botstuff.py
+++ Cogs/botstuff/botstuff.py
@@ -1,8 +1,9 @@
-from datetime import datetime
 import discord
 from discord.ext import commands
 
+import os
 import time
+import inspect
 
 from Core.Utils import get_uptime
 
@@ -10,8 +11,8 @@ class BotStuff(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
     
-    @commands.command(name = "ping", help = "View the ping of the bot", brief = "Take a wild guess")
     @commands.cooldown(2, 5, commands.BucketType.user)
+    @commands.command(name = "ping", help = "View the ping of the bot", brief = "Take a wild guess")
     async def ping(self, ctx: commands.Context):
         async with ctx.typing():
             start = time.perf_counter()
@@ -23,9 +24,43 @@ class BotStuff(commands.Cog):
 
         await msg.edit(content = "Pong \U0001f3d3", embed = embed)
     
-    @commands.command(name='uptime', brief = "Bot Uptime")
     @commands.cooldown(2, 5, commands.BucketType.user)
+    @commands.command(name='uptime', brief = "Bot Uptime")
     async def uptime(self, ctx: commands.Context):
         """Gets the uptime of the bot"""
         uptime_string = get_uptime(self.bot)
         await ctx.channel.send(f'{self.bot.user} has been up for {uptime_string}.\nSince <t:{round(self.bot.launch_ts)}>')
+    
+    @commands.command(brief = "Get Command Source")
+    async def source(self, ctx, *, command: str = None):
+        """Displays my full source code or for a specific command.
+        To display the source code of a subcommand you can separate it by
+        periods, e.g. permissions.add for the add subcommand of the permissions command
+        or by spaces.
+        """
+        source_url = self.bot.config['github']['source']
+        branch = self.bot.config['github']['branch']
+
+        if command is None:
+            return await ctx.send(source_url)
+
+        if command == 'help':
+            src = type(self.bot.help_command)
+            module = src.__module__
+            filename = inspect.getsourcefile(src)
+        else:
+            obj = self.bot.get_command(command.replace('.', ' '))
+            if obj is None:
+                return await ctx.send('Could not find command.')
+
+            # since we found the command we're looking for, presumably anyway, let's
+            # try to access the code itself
+            src = obj.callback.__code__
+            module = obj.callback.__module__
+            filename = src.co_filename
+
+        lines, firstlineno = inspect.getsourcelines(src)
+        location = os.path.relpath(filename).replace('\\', '/')
+
+        final_url = f'<{source_url}/blob/{branch}/{location}#L{firstlineno}-L{firstlineno + len(lines) - 1}>'
+        await ctx.send(final_url)
